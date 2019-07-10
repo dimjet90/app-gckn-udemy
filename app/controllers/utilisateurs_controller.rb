5319e5ec64f6069d26732c046c5045ba00ebf36b
dimjet90 <dim.jetienne@gmail.com>
2019-07-10 16:38:38 +0200

    Ajout des messages d'erreurs à notre formulaire | Close #6

--- app/controllers/utilisateurs_controller.rb
+++ app/controllers/utilisateurs_controller.rb
@@ -7,4 +7,20 @@ class UtilisateursController < ApplicationController
   def new
     @utilisateur = Utilisateur.new
   end
+
+  def create 
+    @utilisateur = Utilisateur.new(utilisateur_params)
+    if @utilisateur.save
+      # Sauvegarde confirmé <= true
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def utilisateur_params
+    params.require(:utilisateur).permit(:nom, :email, :password, :password_confirmation)
+  end
+
 end
