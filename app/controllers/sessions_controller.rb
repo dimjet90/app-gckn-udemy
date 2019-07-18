04345d097247ec4cbba5a829d0b2f4f6e51017a6
dimjet90 <dim.jetienne@gmail.com>
2019-07-18 15:31:20 +0200

    Création d'un formulaire de connexion | Close #11

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    utilisateur = Utilisateur.find_by(email: params[:session][:email].downcase)
+    if utilisateur && utilisateur.authenticate(params[:session][:password])
+    else 
+      flash.now[:danger] = "Email ou mot de passe invalide !"
+    render "new"
+    end
+  end
+
+  def destroy
+  end
+
 end
