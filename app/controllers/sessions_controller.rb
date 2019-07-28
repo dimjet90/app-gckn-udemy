35fb3168ca198bb793538aaa7b334f30be97fe25
dimjet90 <dim.jetienne@gmail.com>
2019-07-28 18:16:34 +0200

    Case 'Se souvenir de moi' | Close #18

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     utilisateur = Utilisateur.find_by(email: params[:session][:email].downcase)
     if utilisateur && utilisateur.authenticate(params[:session][:password])
       log_in utilisateur
-      remember utilisateur
+      params[:session][:remember_me] ==  "1" ? remember(utilisateur) : forget(utilisateur)
       redirect_to utilisateur
     else 
       flash.now[:danger] = "Email ou mot de passe invalide !"
