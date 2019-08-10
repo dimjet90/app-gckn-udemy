34493ba464031666785880aec6e8a0b475fed574
dimjet90 <dim.jetienne@gmail.com>
2019-08-10 16:05:54 +0200

    Création du token pour les comptes utilisateurs | Close #25

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,18 @@ Utilisateur.create!(nom: "Dimitri",
                     email: "dimitri-je@hotmail.fr",
                     password: "root1235",
                     password_confirmation: "root1235",
-                    admin: true)
+                    admin: true,
+                    activated: true,
+                    acitvated_at: Time.zone.now)
 
-99.times do |n|
+59.times do |n|
     nom = Faker::Name.name 
     email = "example-#{n + 1}@gmail.com"
     password = "password"
     Utilisateur.create!(nom: nom,
                         email: email,
                         password: password,
-                        password_confirmation: password)
+                        password_confirmation: password,
+                        activated: true,
+                        acitvated_at: Time.zone.now)
     end
