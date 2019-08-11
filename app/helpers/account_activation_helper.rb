141e96413790a36bfd06f23613d18448ee4cc088
dimjet90 <dim.jetienne@gmail.com>
2019-08-11 17:30:34 +0200

    Activation des utilsateurs | Close #27

--- app/helpers/account_activation_helper.rb
+++ app/helpers/account_activation_helper.rb
@@ -1,2 +1,14 @@
-module AccountActivationHelper
+class AccountActivationsController < ApplicationController
+    def edit
+        utilisateur = Utilisateur.find_by(email: params[:email])
+        if utilisateur && !utilisateur.activated? && utilisateur.authenticated?(:acitvation, params[:id])
+            utilisateur.activate
+            log_in user
+            flash[:success] = "Compte activé!"
+            redirect_to utilisateur
+        else
+            flash[:danger] = "Lien d'activation invalide"
+            redirect_to root_url
+        end
+    end
 end
