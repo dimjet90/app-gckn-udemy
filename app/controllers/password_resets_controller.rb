4db08bfe21eb044a8b170ef48a587bc4854563bd
dimjet90 <dim.jetienne@gmail.com>
2019-09-02 18:49:16 +0200

    Création du formulaire de récupération de mot de passe | Close #30

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,19 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
+  def create
+    @utilisateur = Utilisateur.find_by(email: params[:password_reset][:email].downcase)
+    if @utilisateur
+      @utilisateur.create_reset_digest
+      @utlisateur.send_password_reset_email
+      flash[:info]= "Un email a été envoyé avec les instructions pour récupérer votre mot de passe."
+      redirect_to_root_url
+    else
+      flash.now[:danger] = "Adresse email non trouvée"
+      render "new"
+    end
+  end
+
   def edit
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
-  get "sessions/new"
-
   root "statique#accueil"
   get "/contact", to: "statique#contact"
   get "/team", to: "statique#team"
@@ -15,4 +9,5 @@ Rails.application.routes.draw do
   delete "/logout", to: "sessions#destroy"
   resources :utilisateurs
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
